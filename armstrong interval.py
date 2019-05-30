b7bc5e78f17be6656932568a5621bb3f0ad0f368
priya <46022992+priya0106@users.noreply.github.com>
2019-05-30 11:42:36 +0530

    Create armstrong interval.py

---
+++ armstrong interval.py
@@ -0,0 +1,11 @@
+a,b=list(map(int,input().split()))
+for i in range(a,b):
+  sum=0
+  order=len(str(i))
+  temp=i
+  while(temp>0):
+    digit=digit%10
+    sum+=digit**order
+    temp//=10
+  if i==sum:
+    print(sum)
